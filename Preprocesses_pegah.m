0360e986edea5051b3d7c462429a20311429af47
Andreas Andersen <andersen.a.buus@gmail.com>
2024-03-19 10:20:38 +0100

    improved preprossing files for normal subject

--- Preprocesses_pegah.m
+++ Preprocesses_pegah.m
@@ -11,11 +11,8 @@ main_folderpath = main_folderpath_unedited(1:x(end)+17);
 
 
 % Folders path to data 
-folderpath_data = main_folderpath + "data/";
-
-% File path to data
-filepath_CTL_part1 = folderpath_data + "pegah_1to50_13feb.mat";
-filepath_CTL_part2 = folderpath_data + "pegah_1to50_test2_13feb001.mat"; 
+folderpath_data_part1 = main_folderpath + "/Data_MrKick/" + "pegah_1to50_13feb.mat";
+folderpath_data_part2 = main_folderpath + "/Data_MrKick/" + "pegah_1to50_test2_13feb001.mat";
 
 
 
@@ -29,6 +26,7 @@ HOR  = 3;  % Horizontal
 CTL2 = 4; % Control nr 2  
 
 Protocol_All = [CTL];
+steps_tested = [2,4,6];
 
 % Sensor abbreviation
 SOL = 1; % Soleus 
@@ -50,8 +48,8 @@ fprintf('script: Load data and Acquisition Set-Up from Mr Kick');
 % Functions used: [load_EMG_v2()]
 
 % load pre-control
-[SOL_CTL1, TA_CTL1, angle_CTL1, FSR_CTL1] = load_EMG_v2(filepath_CTL_part1); 
-[SOL_CTL2, TA_CTL2, angle_CTL2, FSR_CTL2] = load_EMG_v2(filepath_CTL_part2); 
+[SOL_CTL1, TA_CTL1, angle_CTL1, FSR_CTL1] = load_EMG_v2(folderpath_data_part1); 
+[SOL_CTL2, TA_CTL2, angle_CTL2, FSR_CTL2] = load_EMG_v2(folderpath_data_part2); 
 
 data{CTL,SOL} = [SOL_CTL1; SOL_CTL2];       clear SOL_CTL1 SOL_CTL2; 
 data{CTL,TA}  = [TA_CTL1; TA_CTL2];         clear TA_CTL1 TA_CTL2; 
@@ -81,8 +79,6 @@ fprintf(' ... done \n');
 %% Filtrering and detrend (similar to MR. kick)
 fprintf('script: Filtrering and detrend (similar to MR. kick)'); 
 
-% Functions used: [rectify_filter()], [filt_FSR()]
-
 fc = 40;                            % Cutoff frequency for LowPass filter
 order = 1;                          % Filter order 
 [b,a] = butter(order,fc/(Fs/2));    % Filter coefficient
@@ -90,7 +86,7 @@ order = 1;                          % Filter order
 % rectify and filter EMG. Remove noise in FSR 
 for proto = Protocol_All
     [data{proto,SOL}, data{proto,TA}] = rectify_filter(data{proto,SOL}, data{proto,TA}, b, a);  
-    [data{proto,FSR}] = func_filt_FSR(data{proto,FSR}, "test", false , "limit_pct", 0.95, "gap_size2", 150, 'gap_size3',800); 
+    [data{proto,FSR}] = func_filt_FSR(data{proto,FSR}, "test", false , "limit_pct", 0.95, "gap_size2", 150); 
 end
 
 fprintf(' ... done \n'); 
@@ -116,18 +112,28 @@ end
 % (sweep,9) -  Zero step, rising  
 
 
-[step_index{CTL}(10,:)] = func_step_index_corr('FSR', data{CTL,FSR}, 'direction', 'right', 'edge_num',1, 'move_num', 1, 'sweep',10, 'offset',8500);
-[step_index{CTL}(27,:)] = func_step_index_corr('FSR', data{CTL,FSR}, 'direction', 'right', 'edge_num',3, 'move_num', 1, 'sweep',27, 'offset',8500);
-[step_index{CTL}(35,:)] = func_step_index_corr('FSR', data{CTL,FSR}, 'direction', 'right', 'edge_num',1, 'move_num', 1, 'sweep',35, 'offset',8500);
-[step_index{CTL}(40,:)] = func_step_index_corr('FSR', data{CTL,FSR}, 'direction', 'right', 'edge_num',1, 'move_num', 1, 'sweep',40, 'offset',8500);
-[step_index{CTL}(44,:)] = func_step_index_corr('FSR', data{CTL,FSR}, 'direction', 'right', 'edge_num',1, 'move_num', 1, 'sweep',44);
-[step_index{CTL}(50,:)] = func_step_index_corr('FSR', data{CTL,FSR}, 'direction', 'right', 'edge_num',1, 'move_num', 0, 'sweep',50, 'offset',8000);
-[step_index{CTL}(85,:)] = func_step_index_corr('FSR', data{CTL,FSR}, 'direction', 'right', 'edge_num',2, 'move_num', 1, 'sweep',85, 'offset',8500);
-[step_index{CTL}(87,:)] = func_step_index_corr('FSR', data{CTL,FSR}, 'direction', 'right', 'edge_num',7, 'move_num', 1, 'sweep',87, 'offset',8500);
+[step_index{CTL}(10,:)] = func_step_index_corr('FSR', data{CTL,FSR}, 'edge_num',1, 'move_num', 1, 'sweep',10, 'offset',8500);
+[step_index{CTL}(27,:)] = func_step_index_corr('FSR', data{CTL,FSR}, 'edge_num',3, 'move_num', 1, 'sweep',27, 'offset',8500);
+[step_index{CTL}(35,:)] = func_step_index_corr('FSR', data{CTL,FSR}, 'edge_num',1, 'move_num', 1, 'sweep',35, 'offset',8500);
+[step_index{CTL}(40,:)] = func_step_index_corr('FSR', data{CTL,FSR}, 'edge_num',1, 'move_num', 1, 'sweep',40, 'offset',8500);
+[step_index{CTL}(44,:)] = func_step_index_corr('FSR', data{CTL,FSR}, 'edge_num',1, 'move_num', 1, 'sweep',44);
+[step_index{CTL}(50,:)] = func_step_index_corr('FSR', data{CTL,FSR}, 'edge_num',1, 'move_num', 0, 'sweep',50, 'offset',8000);
+[step_index{CTL}(85,:)] = func_step_index_corr('FSR', data{CTL,FSR}, 'edge_num',2, 'move_num', 1, 'sweep',85, 'offset',8500);
+[step_index{CTL}(87,:)] = func_step_index_corr('FSR', data{CTL,FSR}, 'edge_num',9, 'move_num', 2, 'sweep',87, 'offset',8500);
+
+% Find falling edge and correct it
+if true 
+    for sweep = 1:size(data{CTL, ANG},1)
+        y = data{CTL, ANG}(sweep,:); 
+        [pks,locs] = findpeaks(y, 'MinPeakProminence',3,'MinPeakDistance',500, 'Annotate','extents'); 
+        step_index{CTL}(sweep, [2,4,6]) = flip(locs(end-2:end)); 
+    end 
+end 
+ 
 
 % >>>> TEST CODE <<<<
-if true 
-    proto = CTL; % CTL[], HOR[x], VER[], CTL2
+if false 
+    proto = CTL; 
     loop = true; sweep = 1; 
     prompt = "Continue, press >c<" + newline + "Quite, press >q<"+ newline + "Change sweep number, press >t<"+ newline;
     figure(2); 
@@ -148,16 +154,25 @@ if true
         plot([step_index{proto}(sweep,fall), step_index{proto}(sweep,fall)],[1 4], "LineWidth",2, "Color", "red")
 
         [rise, fall] = func_find_edge(2); 
+        idxR = step_index{proto}(sweep,rise); 
+        idxF = step_index{proto}(sweep,fall); 
+        text(idxR,5.5,"Step 2")
         plot([step_index{proto}(sweep,rise), step_index{proto}(sweep,rise)],[1 4], "LineWidth",2, "Color", "black")
         plot([step_index{proto}(sweep,rise), step_index{proto}(sweep,fall)],[2.5 2.5], "LineWidth",2, "Color", "black")
         plot([step_index{proto}(sweep,fall), step_index{proto}(sweep,fall)],[1 4], "LineWidth",2, "Color", "red")
 
         [rise, fall] = func_find_edge(4); 
+        idxR = step_index{proto}(sweep,rise); 
+        idxF = step_index{proto}(sweep,fall); 
+        text(idxR,5.5,"Step 4")
         plot([step_index{proto}(sweep, rise), step_index{proto}(sweep,rise)],[1 4], "LineWidth",2, "Color", "green")
         plot([step_index{proto}(sweep,rise), step_index{proto}(sweep,fall)],[2.5 2.5], "LineWidth",2, "Color", "black")
         plot([step_index{proto}(sweep,fall), step_index{proto}(sweep,fall)],[1 4], "LineWidth",2, "Color", "blue")
     
         [rise, fall] = func_find_edge(6); 
+        idxR = step_index{proto}(sweep,rise); 
+        idxF = step_index{proto}(sweep,fall); 
+        text(idxR,5.5,"Step 6")
         plot([step_index{proto}(sweep, rise), step_index{proto}(sweep,rise)],[1 4], "LineWidth",2, "Color", "black")
         plot([step_index{proto}(sweep,rise), step_index{proto}(sweep,fall)],[2.5 2.5], "LineWidth",2, "Color", "black")
         plot([step_index{proto}(sweep,fall), step_index{proto}(sweep,fall)],[1 4], "LineWidth",2, "Color", "red")
@@ -190,6 +205,74 @@ if true
 end
 
 fprintf(' ... done \n'); 
+
+%% Manually readjust 
+fprintf('script: Readjust data to Local Peak instead of FSR .  .  .  .  .  '); tic
+
+readjust = true; 
+show_gui = true; 
+
+if readjust
+    % Check if an already defined offset file exist for subject 
+    filename =  "/Subject_offsets/" + SubjectName + "_offset.mat";
+    filepath = fullfile(main_folderpath, filename);
+    if exist(filepath, 'file') == 2
+        load(filepath) % return 4x1 cell array called 'offset'
+        step_index = offset; 
+    else 
+        fprintf(2,'No offset file exist \n'); 
+    end
+end
+     
+
+if show_gui
+    fprintf('\n Re-adjust gui - [Waiting for user input] \n')
+
+    filename =  "/Subject_offsets/" + SubjectName + "_offset.mat";
+    filepath = fullfile(main_folderpath, filename);
+    if exist(filepath, 'file') == 2
+        load(filepath) % return 4x1 cell array called 'offset'
+        step_index = offset; 
+    end
+
+    offset = [];    % preparer for new input    
+    %readjustFSR     % open gui
+    readjustFSR
+    pause           % wait for user input 
+    
+    if ~isempty(offset)
+        if exist(filepath, 'file') == 2 
+            prompt = newline + "Want to over save. YES: press >y<. NO, press >n<"+ newline;
+            correctInput = false; 
+            while correctInput == false     % Wait for correct user input
+                switch input(prompt, 's')   % Save user input
+                    case "y"
+                        correctInput = true; 
+                        oversave = true; 
+                    case "n"
+                        correctInput = true; 
+                        oversave = false;
+                    otherwise
+                        correctInput = false;
+                        warning("Input not accepted")
+                end 
+            end 
+        else 
+            oversave = true; 
+        end 
+
+        if oversave
+            step_index = offset; 
+            save(filepath,'offset')
+            disp("Data saved or oversaved")
+        end         
+    end 
+    fprintf('gui done \n')
+end 
+
+
+
+
 
 %% Save Data 
 fprintf('script: Save data'); 
